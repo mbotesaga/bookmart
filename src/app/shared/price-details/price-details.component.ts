1d5195beb80d04c728d5454e5b1f407bd77b483b
Cleg <ewihinnovation@gmail.com>
2024-08-10 11:48:56 +0700

    price-details

--- src/app/shared/price-details/price-details.component.ts
+++ src/app/shared/price-details/price-details.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../core/cart.service';
 
 @Component({
   selector: 'app-price-details',
   templateUrl: './price-details.component.html',
   styleUrl: './price-details.component.css'
 })
-export class PriceDetailsComponent {
+export class PriceDetailsComponent implements OnInit {
+  cartItems: any[] = [];
+  cartItemsPrice: any;
+  cartItemsDiscount: any;
+  deliveryCharge: any;
+  
+  constructor(private cartService:CartService){}
 
+ngOnInit(): void {
+  this.cartItems = this.cartService.getAllCartItems();
+  this.getBillingDetails();
+  this.cartService.cartSubject.subscribe((items:any) => {
+    this.cartItems = items;
+    this.getBillingDetails();
+  })
+}
+
+getBillingDetails(){
+  let billingDetails = this.cartService.getBilling(this.cartItems);
+  this.cartItemsPrice = billingDetails.price;
+  this.cartItemsDiscount = billingDetails.discount;
+  this.deliveryCharge = billingDetails.delivery;
+}
 }
